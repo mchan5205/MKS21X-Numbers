1789491c65b70352cc5ecba94a66903939e044ae
Michael Chan <mchan10@stuy.edu>
2018-10-15 22:59:37 -0400

    added methods for both classes

---
+++ RationalNumber.java
@@ -0,0 +1,12 @@
+public class RationalNumber{
+	private double numerator;
+	private double denominator
+	public RealNumber(double newnumerator, double newdenominator){
+	}	
+	public String toString(){
+1	}
+	public double getrealnumber(){
+	}	
+	public boolean compare(RationalNumber other){
+	}	
+}	
---
+++ RealNumber.java
@@ -0,0 +1,9 @@
+public class RealNumber{
+	private double realnumber;
+	public RealNumber(double newrealnumber){
+	}	
+	public String toString(){
+1	}
+	public double getrealnumber(){
+	}	
+}	
